b8d1272b57864a604edb64ef6c48249d3bf2050a
Kaarubea <karl.ribz.da.great@gmail.com>
2023-09-23 01:17:22 +1200

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your marks: ");
+        string marks = Console.ReadLine();
+        int score = int.Parse(marks);
+        string letter = "";
+        string sign = "";
+        int lastdigit = score % 10;
+
+        // Console.WriteLine($"Last digit is:  {lastdigit}");
+
+        if (score >= 90)
+        {
+            letter = "A";
+        } 
+
+        else if (score >= 80 && score < 90 && lastdigit >= 7)
+        {
+            letter = "B";
+            sign = "+";
+        }
+        
+        else if (score >= 80 && score < 90 && lastdigit < 3)
+        {
+            letter = "B";
+            sign = "-";
+        }
+
+        else if (score >= 70 && score < 80 && lastdigit >= 7)
+        {
+            letter = "C";
+            sign = "+";
+        }
+        
+        else if (score >= 70 && score < 80 && lastdigit < 3)
+        {
+            letter = "C";
+            sign = "-";
+        }
+
+        else if (score >= 60 && score < 70 && lastdigit >= 7)
+        {
+            letter = "D";
+            sign = "+";
+        }
+        
+        else if (score >= 60 && score < 70 && lastdigit < 3)
+        {
+            letter = "D";
+            sign = "-";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is: {letter}{sign}");
+        
+        if (score >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#");
+
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
+
+        string school = "BYU-Idaho";
+        Console.WriteLine($"I am studying at {school}.");
+
+        string World = "Eritebwaa";
+        Console.WriteLine($"My wife is {World}.");
     }
+
 }
